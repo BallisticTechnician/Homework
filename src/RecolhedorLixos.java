5cca472c3f2dd4d61c7921595098ca6140c7dc8e
Celso Campos <ballistictechnician@outlook.com>
2021-06-14 01:18:47 -0300

    Exercicio do drone corrigido

--- src/RecolhedorLixos.java
+++ src/RecolhedorLixos.java
@@ -13,17 +13,23 @@ public class RecolhedorLixos {
 	System.out.println("Entre com o peso do lixo 3 :");
 	peso3 = leitor.nextInt();
 	System.out.println("Recolher os lixos na seguinte ordem : ");
-	if(peso1<=peso2) {
-		if(peso1<=peso3) {
-			if(peso2<=peso3) {
-				System.out.println("1 - lixo 1, 2 - lixo 2 e 3 - lixo 3");
-				}
-			else {System.out.println("1 - lixo 1, 2 - lixo 3 e 3 - lixo 2");}}
-		else {System.out.println("1 - lixo 3, 2 - lixo 1 e 3 - lixo 2");}
-		if(peso3<=peso2) {
-		System.out.println("1 - lixo 3, 2 - lixo 2 e 3 - lixo 1");}
-		else {System.out.println("1 - lixo 2, 2 - lixo 3 e 3 - lixo 1");}
-		if (peso3<=peso2){System.out.println("1 - lixo 3, 2 - lixo 2 e 3 - lixo 1");}
-	}
-	}
-	}
+	if(peso1<=peso2 && peso1<=peso3 && peso2<=peso3) {
+		System.out.println("1 - lixo 1, 2 - lixo 2 e 3 - lixo 3");
+		}
+		else if(peso1<=peso2 && peso1<=peso3 && peso3<=peso2) {
+			System.out.println("1 - lixo 1, 2 - lixo 3 e 3 - lixo 2");
+		}
+		else if (peso2<=peso1 && peso2<=peso3 && peso1<=peso3) {
+			System.out.println("1 - lixo 2, 2 - lixo 1 e 3 - lixo 3");
+		}
+		else if (peso2<=peso1 && peso2<=peso3 && peso3<=peso1) {
+			System.out.println("1 - lixo 2, 2 - lixo 3 e 3 - lixo 1");
+		}
+		else if (peso3<=peso1 && peso3<=peso2 && peso2<=peso1) {
+			System.out.println("1 - lixo 3, 2 - lixo 2 e 3 - lixo 1");
+		}
+		else if (peso3<=peso1 && peso3<=peso2 && peso1<=peso2) {
+			System.out.println("1 - lixo 3, 2 - lixo 1 e 3 - lixo 2");
+		}
+}
+}
